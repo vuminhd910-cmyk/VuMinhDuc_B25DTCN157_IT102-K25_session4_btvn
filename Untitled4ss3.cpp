c87797a5200ff6db43a5c074029ddf28e2b5e6c2
vuminhd910-cmyk <vuminhd910@gmail.com>
2025-10-27 22:41:29 +0700

    Update Untitled4ss3.cpp

--- Untitled4ss3.cpp
+++ Untitled4ss3.cpp
@@ -1,47 +1,46 @@
 #include <stdio.h>
 int main(){
-	int n;
-	printf("nhap cac thang tu thang 1-12:");
-	scanf("%d",&n);
-	
+	 int n;
+	printf("bat dau nhap cac thang tu 1-12:");
+	         scanf("%d",&n);
 	switch(n){
-		case 1:
+		    case 1:
 			printf("Thang 1 co 31 ngay");
 			break;
-		case 2:
-			printf("Thang 2 co 28 ngay hoac 29 ng‡y(nam nhuan)");
+		    case 2:
+			printf("Thang 2 co 28 ngay hoac 29 ng√†y");
 			break;
-		case 3:
+		    case 3:
 			printf("Thang 3 co 31 ngay");
 			break;
-		case 4:
+		    case 4:
 			printf("Thang 4 co 30 ngay");
 			break;
-		case 5:
+		    case 5:
 			printf("Thang 5 co 31 ngay");
 			break;
-		case 6:
+		    case 6:
 			printf("Thang 6 co 30 ngay");
 			break;
-		case 7:
+		    case 7:
 			printf("Thang 7 co 31 ngay");
 			break;
-		case 8:
+		    case 8:
 			printf("Thang 8 co 31 ngay");
 			break;
-		case 9:
+		    case 9:
 			printf("Thang 9 co 30 ngay");
 			break;
-		case 10:
+		    case 10:
 			printf("Thang 10 co 31 ngay");
 			break;
-		case 11:
+		    case 11:
 			printf("Thang 11 co 30 ngay");
 			break;
-		case 12:
+		    case 12:
 			printf("Thang 12 co 31 ngay");
 			break;	
-		default:
+		    default:
 			printf("thang khong hop le");
 	
 		
@@ -51,4 +50,5 @@ int main(){
 	
 
 }
+
 
