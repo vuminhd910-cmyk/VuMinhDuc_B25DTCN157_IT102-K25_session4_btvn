7011d157d5e38952baddab776e5a6659ba017fc0
vuminhd910-cmyk <vuminhd910@gmail.com>
2025-10-27 22:54:05 +0700

    Update Untitled10ss4.cpp

--- Untitled10ss4.cpp
+++ Untitled10ss4.cpp
@@ -1,21 +1,22 @@
 #include <stdio.h>
 int main(){
-	int a,b,c,fusion;
-	printf("nhap so nguyen thu nhat:");
-	scanf("%d",&a);
-	printf("nhap so nguyen thu hai:");
-	scanf("%d",&b);
-	printf("nhap so nguyen thu ba:");
-	scanf("%d",&c);
-	if(a>b){
+int a,b,c,fusion;
+	    printf("nhap so nguyen thu nhat:");
+	           scanf("%d",&a);
+	    printf("nhap so nguyen thu hai:");
+	           scanf("%d",&b);
+	    printf("nhap so nguyen thu ba:");
+	           scanf("%d",&c);
+if(a>b){
 		fusion=a,a=b,fusion=b;
 	}
-	if(a>c){
+if(a>c){
 		fusion=a,a=c,fusion=c;
 	}
-	if(b>c){
+if(b>c){
 		fusion=b,b=c,fusion=c;
 	}
-	printf("sap xep theo thu tu tang dan:%d,%d,%d",a,b,c);
+	    printf("sap xep theo thu tu tang dan:%d,%d,%d",a,b,c);
 	
 }
+
