3711b5f13d8b564b449b44f410cc884afab4a2dc
vuminhd910-cmyk <vuminhd910@gmail.com>
2025-10-27 22:52:31 +0700

    Update Untitled8ss4.cpp

--- Untitled8ss4.cpp
+++ Untitled8ss4.cpp
@@ -2,14 +2,15 @@
 
 int main(){
 	int n,e,u;
-	printf("do dai ba canh cua tam giac: ");
-	scanf("%d%d%d",&n,&e,&u);
+	         printf("do dai ba canh cua tam giac: ");
+	        scanf("%d%d%d",&n,&e,&u);
 	
 	if(n+e>u && n+u>e && e+u>n){
-		printf("day la 3 canh cua tam giac");
+		    printf("day la 3 canh cua tam giac");
 	} else{
-		printf("khong phai 3 canh cua tam giac");
+		    printf("khong phai 3 canh cua tam giac");
 		
 	}
 }
+
 
