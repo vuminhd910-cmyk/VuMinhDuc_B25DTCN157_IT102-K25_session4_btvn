fec22ce58252b4c5ac86d392e3474e37f2073789
vuminhd910-cmyk <vuminhd910@gmail.com>
2025-10-27 22:34:18 +0700

    Update Untitled3ss4.cpp

--- Untitled3ss4.cpp
+++ Untitled3ss4.cpp
@@ -1,15 +1,16 @@
 #include <stdio.h>
 int main(){
-	int n;
-	printf("nhap so nguyen n:");
+	int a;
+	printf("nhap so nguyen a:");
 	scanf("%d",&n);
-	if(n%3 ==0 &&n%5 ==00)
-		printf("chia het cho 3 va 5");
-		else if(n%3 ==0)
-		printf("chia het cho 3");
-		else if(n%5 ==0)
-		printf("chia het cho 5");
+	if(n%3 ==0 && n%5 ==00)
+printf("so n chia het cho 3 va 5");
+		       else if(n%3 ==0)
+printf("so n chia het cho 3");
+		        else if(n%5 ==0)
+printf("so n chia het cho 5");
 
 
 }
+
 
