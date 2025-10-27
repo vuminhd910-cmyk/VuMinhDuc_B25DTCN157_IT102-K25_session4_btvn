5ac8d94eea22bf6d8793c41d385da5feaad3dde6
vuminhd910-cmyk <vuminhd910@gmail.com>
2025-10-27 22:45:13 +0700

    Update Untilted5ss4.cpp

--- Untilted5ss4.cpp
+++ Untilted5ss4.cpp
@@ -1,29 +1,30 @@
 #include <stdio.h>
 int main(){
-	int chi_so_cu,chi_so_moi,so_dien;
-    float tien;
+	      int chisocu,chisomoi,sodien;
+          float tien;
     printf("nhap chi so cu:");
-    scanf("%d",&chi_so_cu);
+          scanf("%d",&chisocu);
     printf("nhap chi so moi:");
-    scanf("%d",&chi_so_moi);
-    so_dien=chi_so_moi-chi_so_cu;
-    if(so_dien<0){
+          scanf("%d",&chisomoi);
+    sodien=chisomoi-chisocu;
+           if(sodien<0){
     printf("chi so moi lon hon chi so cu");
 }
-    if(so_dien<50)
-        tien=so_dien*10000;
-    else if(so_dien<100)
-        tien=50*10000+(so_dien-50)*15000;
-    else if(so_dien<150)
-        tien=50*10000+50*15000+(so_dien-100)*20000;
-    else if(so_dien<200)
-        tien=50*10000+50*15000+50*20000+(so_dien-150)*25000;
-    else if(so_dien>=200)
-        tien=50*10000+50*15000+50*20000+50*25000+(so_dien-200)*30000;
-    printf("So dien tieu thu:%d kwh\n",so_dien);
-	printf("tien dien phai tra la:%.3f vnd",tien);
+    if(sodien<50)
+              tien=sodien*10000;
+    else if(sodien<100)
+              tien= 50*10000+(sodien-50)*15000;
+    else if(sodien<150)
+              tien= 50*10000+50*15000+(sodien-100)*20000;
+    else if(sodien<200)
+              tien= 50*10000+50*15000+50*20000+(sodien-150)*25000;
+    else if(sodien>=200)
+              tien= 50*10000+50*15000+50*20000+50*25000+(so_dien-200)*30000;
+                      printf("So dien tieu thu:%d kwh\n",so_dien);
+	                  printf("tien dien phai tra la:%.3f vnd\n",tien);
 
 
     
 }
+
 
