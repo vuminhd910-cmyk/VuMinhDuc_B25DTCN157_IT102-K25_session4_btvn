56c2f87788f74e84f6188ff981682123208dba02
vuminhd910-cmyk <vuminhd910@gmail.com>
2025-10-27 22:51:32 +0700

    Update Untitled7ss4.cpp

--- Untitled7ss4.cpp
+++ Untitled7ss4.cpp
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main(){
-	int a;
-	printf("nhap so nam: ");
-	scanf("%d",&a);
-	if((a%4 ==0 &&a%100 !=-0)||(a%400 ==0)){
+int a;
+	    printf("nhap so nam: ");
+scanf("%d",&a);
+	    if((a%4 ==0 &&a%100 !=-0)||(a%400 ==0)){
 		printf("day la nam nhuan");
-	}else{
+	     }else{
 		printf("day khong phai nam nhuan");
-	} 
-} 
+	   } 
+   } 
+
 
